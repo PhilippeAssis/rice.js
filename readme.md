429245e7232aef5501e1714a6956f7337198e846
Philippe Assis <assis@philippeassis.com>
2017-03-27 18:18:15 +0000

    all

--- readme.md
+++ readme.md
@@ -1,14 +1,131 @@
-# Rice.js
+# Ricejs
 #### "Matches everything!"
 
-
 ## What I am?
-Rice.js is a frontend integration framework for javascript applications.
-Its main picture is the architecture of the project, allowing the developer to work with any of the thousands of javascript frameworks on the internet.
+Ricejs is a frontend integration framework for javascript applications. Its main feature is the architecture of the project, allowing the developer to work with any of the thousands of javascript frameworks on the internet.
 
-Rice.js works in direct partnership with Gulp and Yeoman.
+Ricejs works in direct partnership with [Gulp](https://github.com/gulpjs/gulp), [Yeoman](https://github.com/yeoman/yeoman), [Babel](https://github.com/babel/babel) and [Browserify](https://github.com/substack/node-browserify).
 
 ## Install
+### or Ricejs to Yeoman
 ```
-npm install rice-cli
+npm install -g yo generator-ricejs && yo ricejs
 ```
+### or Rice core
+```
+npm install ricejs
+```
+
+## Architecture
+Ricejs guarantees a simple, intuitive and organized file architecture.
+
+```
++-- app
+|   +-- assets
+|   +-- configs
+|   +-- controllers
+|   +-- libs
+|   +-- services
+|   +-- views
+|   +-- app.js
++-- public
++-- node_modules
++-- gulpfiles.js
+```
+
+To start a project with ricejs, once installed, just run ricejs on the terminal. When everything is installed and completed, run gulp live to enable automatic scripting.
+
+### Live reload
+The [livereload](https://github.com/vohof/gulp-livereload) is responsible for dynamically updating web pages. You will need to install extensions for browsers. You can find these extensions at [livereload.com/extensions](http://livereload.com/extensions/).
+
+## ECMAScript 6 e Node Modules 
+Ricejs comes configured by default to work with *ECMAScript 6* carrying your built-in script to *ECMAScript 5*. He uses the *Babel* to do this task.
+
+### Browserify
+Browserify guarantees the use of npm modules in your application, learn more at [node-browserify] (https://github.com/substack/node-browserify)
+
+## Config method's
+The Ricejs settings handling method allows you to easily manipulate your application settings.
+They come with an identifier called "String to tree object".
+
+### String to tree object
+
+Here is a demonstration of tree structure of a configuration object
+```json
+{
+    "page": {
+        "name" : "home"
+    },
+    "user": {
+        "id": 123,
+        "name": "Assis"
+    }
+}
+```
+
+Traditionally you would get the value of page.name so `config.page.name`, and this is how you will indicate the object to be handled (add, get, set) with the configuration methods.
+
+### addConfig, getConfig, setConfig
+#### add
+
+Let's add some data to our configuration:
+```javascript
+ Rice.addConfig("page.name", "home")
+ Rice.addConfig("user.name", "Assis")
+```
+Now, let's modify them
+```javascript
+ Rice.setConfig("page.name", "contact")
+ Rice.setConfig("user.name", "Philippe")
+```
+In the end, let's get it.
+```javascript
+ Rice.getConfig("page.name") // contact
+ Rice.setConfig("user.name") // Philippe
+ Rice.setConfig() // Complete tree of the config object
+```
+
+## Controllers
+All controllers must be available in `app / controllers`, using the suffix **Controller.js**.
+Controllers are modules that allow you to organize your application by task groups.
+
+To start a controller, simply declare `Rice.controller ('CONTROLLER_NAME')`.
+
+## Services
+The services must be available in `app/services`, using the suffix **Service.js**.
+The services are modules intended to work with an application service.
+
+Some methods must be available in this module, such as **start**, **stop**, **restart**, **reload**.
+Its default structure is this:
+```javascript
+import Rice from 'ricejs'
+
+exports function myService(){
+    this.start = () => {/*...*/} // Required
+    
+    this.restart = () => {/*...*/} 
+    
+    this.reload = () => {/*...*/}
+    
+    this.stop = () => {/*...*/}
+    
+    return this;
+}
+```
+The **start method** is required only one..
+
+## All methods
+ - **addConstructor(build)**: Adds a counter to start along with the rice. Used mainly for plugins.
+ - **init()**: Rice builders start.
+ - **addConfig(name, value)**: Add a setting.
+ - **setConfig(name, value)**: Change a setting.
+ - **getConfig()**: Get value of a setting.
+ - **add(name, value)**: Adds a property to the root of the Rice object.
+ - **build(name, build)**: To creation of plugins.
+ - **cache(name, value)**: Specific for temporary data.
+ - **global()**: Specific to global data that may be wanted throughout the application.
+ - **import()**: Imports a library/module within the structure of rice.
+ - **controller()**: Start a controller.
+ - **service()**: Start a service.
+ - **lib()**: Start a library.
+ - **allServices(method)**: Would call one method on every imported service
--- src/rice.js
+++ src/rice.js
@@ -6,22 +6,44 @@ var __RiceData = {
         "cache": {},
         "constructors": [],
         "controllers": {},
-        "config": {
-            "test": {
-                "ok": true
-            }
-        },
+        "config": {},
         "build": {}
     }
 }
 
-function RiceCore() {
-    var rice = {
-        "service": {},
-        "controller": {}
+function mapping(obj, item, value) {
+    if (typeof item == "string") {
+        var item = item.split(".")
+        var _this = obj;
+
+        if (!value) {
+            for (let i = 0; i < item.length; i++) {
+                _this = _this[item[i]]
+            }
+        }
+        else {
+            item.reverse();
+            var result = {
+                [item[0]]: value
+            };
+
+            for (let i = 1; i < (item.length - 1); i++) {
+                result = {
+                    [item[i]]: result
+                }
+            }
+
+            _this[item[(item.length - 1)]] = result;
+        }
+
+        return _this;
     }
 
-    rice.name = "Rice!"
+    return obj;
+}
+
+module.exports = (function(appPath = "app") {
+    var rice = {};
 
     rice._getData = () => {
         return __RiceData;
@@ -48,7 +70,7 @@ function RiceCore() {
             if (mapping(__RiceData._.config, name)) {
                 return console.error(`There is already an item named "${name}" in config. To update this item use setConfig ()`)
             }
-            
+
             mapping(__RiceData._.config, name, value)
         }
 
@@ -90,8 +112,6 @@ function RiceCore() {
         return rice;
     }
 
-
-
     rice.build = (name, build) => {
         if (!__RiceData._.build[name]) {
             __RiceData._.build[name] = {}
@@ -120,7 +140,7 @@ function RiceCore() {
         var done = build.apply(rice)
 
         if (done) {
-            return Rice = done;
+            __RiceData._.constructors.push(done)
         }
 
         return rice;
@@ -144,91 +164,41 @@ function RiceCore() {
         }
     }
 
-    rice.addService = (name, value) => {
-        if (__RiceData._.services[name]) {
-            return console.error(`Can not create the "${name}" service. Another service with this name already exists`)
-        }
-
-        __RiceData._.services[name] = value;
+    rice.import = (path, name, ...options) => {
+        var module = require(`./${appPath}/${path}/${name}`)
+        return module[name].apply(this, options)
     }
 
-    rice.initService = (name, ...args) => {
-        if (!__RiceData._.services[name]) {
-            return console.error(`There is no registered service called "${name}"`)
-        }
-
-        rice.service[name] = __RiceData._.services[name].apply(null, args);
-
-        if (rice.service[name].init) {
-            rice.service[name].init();
-        }
+    rice.controller = (name, ...options) => {
+        var module = require(`./${appPath}/controllers/${name}Controller.js`)
+        return module[name].apply(this, options)
     }
 
-    rice.initAllServices = (args = {}) => {
-        for (let name in __RiceData._.services) {
-            var arg = [name]
+    rice.service = (name, ...options) => {
+        if (__RiceData._.servicesLoaded[name]) {
+            return __RiceData._.servicesLoaded[name]
+        }
 
-            if (args[name]) {
-                if (Array.isArray(args[name])) {
-                    args[name].map((item) => {
-                        arg.push(args[item])
-                    })
-                }
-                else {
-                    arg.push(args[name])
-                }
+        var module = require(`./${appPath}/services/${name}Service.js`)
+        __RiceData._.servicesLoaded[name] = module[name].apply(this, options)
+        return __RiceData._.servicesLoaded[name];
+    }
+
+    rice.lib = (name, ...options) => {
+        var module = require(`./${appPath}/libs/${name}.js`)
+        return module[name].apply(this, options)
+    }
+    
+    rice.allServices = (method) => {
+        for (let key in __RiceData._.servicesLoaded) {
+            if (__RiceData._.servicesLoaded[key][method]) {
+                __RiceData._.servicesLoaded[key][method]()
             }
-
-            rice.initService.apply(null, arg)
-        }
-    }
-
-    rice.stopService = (name) => {
-        if (rice.service[name]) {
-            if (rice.service[name].stop) {
-                rice.service[name].stop(() => {
-                    delete rice.service[name];
-                })
-            }
-            else {
-                delete rice.service[name]
-            }
-
-            delete __RiceData._.services[name]
-        }
-    }
-
-    rice.stopAllServices = function(name) {
-        for (let name in rice.service) {
-            rice.stopService(name)
-        }
-    }
-
-    rice.addController = function(name, controller) {
-        __RiceData._.controllers[name] = controller;
-    }
-
-    rice.controller = function(name, ...args) {
-        try {
-            var control = __RiceData._.controllers[name]
-            return control.apply(null, args);
-        }
-        catch (e) {
-            if (!__RiceData._.controllers[name]) {
-                return console.error(`The controller "${name}" has not been registered. Use Rice.addController("${name}", function(){...}) to register it.`)
-            }
-            else {
-                console.error(e)
+            else{
+                console.error(`The ${key} service does not contain the start method`)
             }
         }
     }
 
     return rice;
-}
-
-if (typeof module == "object" && typeof module.exports == "object") {
-    module.exports = new RiceCore();
-}
-else {
-    var Rice = new RiceCore();
-}
+})()
